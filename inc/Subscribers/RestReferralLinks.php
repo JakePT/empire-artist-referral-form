84a2166d1867e0e81563503af2c9d297fbdb680f
Jacob Peattie <jakept@me.com>
2020-02-07 19:31:10 +1100

    Move get referral link function into REST controller for now.

--- inc/Subscribers/RestReferralLinks.php
+++ inc/Subscribers/RestReferralLinks.php
@@ -5,7 +5,6 @@ use WP_Error;
 use WP_REST_Request;
 use WP_REST_Response;
 use EmpireArtist\ReferralForm\Interfaces\HasActions;
-use function EmpireArtist\ReferralForm\get_referral_link_by_email;
 
 class RestReferralLinks implements HasActions {
 	/**
@@ -47,6 +46,50 @@ class RestReferralLinks implements HasActions {
 	public function get( WP_REST_Request $request ) {
 		$email_address = $request->get_param( 'email_address' );
 
-		return get_referral_link_by_email( $email_address );
+		return $this->get_referral_link_by_email( $email_address );
+	}
+
+	/**
+	 * Find a referral link based on a provided email address.
+	 *
+	 * @param string $email_address An email address.
+	 * @return string|WP_Error A referral link URL on success, or WP_Error on failure.
+	 */
+	private function get_referral_link_by_email( string $email_address ) {
+		$user = get_user_by( 'email', $email_address );
+
+		if ( ! $user ) {
+			return new WP_Error(
+				'empire_artist_referral_form_email_not_found',
+				 __( 'No referrer found with that email address', 'empire-artist-referral-form' ),
+	 			[ 'status' => 400 ]
+			);
+		}
+
+		$base_url = get_option( 'rs_static_generate_link' );
+
+		if ( ! $base_url ) {
+			return new WP_Error(
+				'empire_artist_referral_form_no_base_url',
+				 __( 'No referral link found.', 'empire-artist-referral-form' ),
+	 			[ 'status' => 400 ]
+			);
+		}
+
+		/**
+		 * Generate referral link. Method based on code in SUMO Reward Points 24.3.
+		 */
+		$refer_by_username = ( '1' === get_option( 'rs_generate_referral_link_based_on_user' ) );
+		$ip_restricted     = ( 'yes' === get_option( 'rs_restrict_referral_points_for_same_ip' ) );
+
+		$query_args = [
+			'ref' => $refer_by_username ? $user->user_login : $user->ID,
+		];
+
+		if ( $ip_restricted ) {
+			$query_args['ip']  = base64_encode( get_referrer_ip_address() );
+		}
+
+		return add_query_arg( $query_args , $base_url ) ;
 	}
 }
